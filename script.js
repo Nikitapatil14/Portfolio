84c79ce448621c71fd9b8b36ea8bb93630cedf60
Nikita Patil <nikitapatil930959@gmail.com>
2026-06-20 15:40:32 +0530

    Add button interaction to alert user

---
+++ script.js
@@ -0,0 +1,5 @@
+// button interaction
+document.querySelector(".btn").addEventListener("click", () => {
+  document.getElementById("projects");
+  alert("Scroll down or add projects section 🚀");
+});
